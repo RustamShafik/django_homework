ba01049bc630fe9ae7ee06b2d053df6321595511
Rustam Shafikov <mitaleko@gmail.com>
2025-05-05 11:32:08 +0500

    Homework 3 Task 4 done

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Category, Product
+
+
+# Зарегистрируйте модели Product и Category в админке.
+# Настройте отображение для моделей: Для Category выведите id и name
+# в списке. Для Product выведите id , name , price и category в списке.
+# Настройте фильтрацию продуктов по категории. Настройте поиск
+# по полям name и description .
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name",)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "purchase_price", "category",)
+    list_filter = ("category",)
+    search_fields = ("name", "description")
